eba68306dcc57de694b9bbc4b5751c44655c2671
br4ska <sven.steffan@gmail.com>
2017-11-18 16:21:05 +0100

    - added ViewEditMode-functionality and some sample data to the contactinformation panels

--- Frontend/src/js/components/PersonProfile/views/BusinessInformation.react.js
+++ Frontend/src/js/components/PersonProfile/views/BusinessInformation.react.js
@@ -3,39 +3,116 @@
  */
 import React from 'react';
 
+import Switch from '../../../widgets/base/Switch.react.js';
 import InputField from '../../../widgets/base/InputField.react.js';
 import ComboBox from '../../../widgets/base/ComboBox.react.js';
 import PersonContacts from './PersonContacts.react.js';
 
 export default class BusinessInformation extends React.Component {
 
-    render() {
+    toggleEditMode;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            editMode: false
+        };
+
+        this.toggleEditMode = () => this.setState({
+            editMode: !this.state.editMode
+        });
+    }
+
+    renderViewForm() {
+        let inputLabelContainerStyle = {
+            textAlign: "right",
+            fontSize: "12px"
+        };
+
+        let valueLabel = {
+            textAlign: "left",
+            fontSize: "12px",
+            borderBottom: "solid 1px lightgrey"
+        };
+
+        return (
+            <div className="panel-body">
+                <form className="form-horizontal">
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Kürzel:</label></div>
+                        <div className="col-sm-9" style={valueLabel}>br4sk4</div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Eintrittsdatum:</label></div>
+                        <div className="col-sm-9" style={valueLabel}>---</div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Firma:</label></div>
+                        <div className="col-sm-9" style={valueLabel}>NevSoft</div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+
+    renderEditForm() {
         let inputLabelContainerStyle = {
             textAlign: "right",
             paddingTop: "6px",
             fontSize: "12px"
         };
 
+        const comboboxData = [
+            'NevSoft',
+            'Alpha AG',
+            'Beta GmbH',
+            'Gamma, Co. Ltd.',
+            'Delta, Inc.',
+            'Epsilon AG',
+            'Lambda AG',
+            'Rho GmbH',
+            'Omega, Inc.',
+        ];
+
+        return (
+            <div className="panel-body">
+                <form className="form-horizontal">
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Kürzel:</label></div>
+                        <div className="col-sm-9"><InputField/></div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Eintrittsdatum:</label></div>
+                        <div className="col-sm-9"><InputField/></div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Firma:</label></div>
+                        <div className="col-sm-9"><ComboBox showSearchBox data={comboboxData}/></div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Führungskraft:</label></div>
+                        <div className="col-sm-9"><Switch/></div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Ausbilder:</label></div>
+                        <div className="col-sm-9"><Switch/></div>
+                    </div>
+                </form>
+                <PersonContacts {...this.props}/>
+            </div>
+        );
+    }
+
+    render() {
+        const switchEditModeIconClass = (this.state.editMode === true) ? "glyphicon glyphicon-eye-open" : "glyphicon glyphicon-pencil";
+
         return (
             <div className="panel panel-info" style={{width: "1000px", margin: "0 auto", marginTop: "15px"}}>
-                <div className="panel-heading"><strong>Betriebliche Kontaktinformationen</strong></div>
-                <div className="panel-body">
-                    <form className="form-horizontal">
-                        <div className="form-group">
-                            <div className="col-sm-2" style={inputLabelContainerStyle}><label>Kürzel:</label></div>
-                            <div className="col-sm-9"><InputField width="100%"/></div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-sm-2" style={inputLabelContainerStyle}><label>Eintrittsdatum:</label></div>
-                            <div className="col-sm-9"><InputField width="100%"/></div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-sm-2" style={inputLabelContainerStyle}><label>Firma:</label></div>
-                            <div className="col-sm-9"><ComboBox width="100%" data={['Beta AG', 'ABC GmbH', 'XYZ GmbH']}/></div>
-                        </div>
-                    </form>
-                    <PersonContacts {...this.props}/>
+                <div className="panel-heading" style={{position: "relative"}}>
+                    <strong>Betriebliche Kontaktinformationen</strong>
+                    <div className={switchEditModeIconClass} style={{position: "absolute", fontSize: "16px", top: "14px", right: "14px"}} onClick={this.toggleEditMode}/>
                 </div>
+                {(this.state.editMode === true) ? this.renderEditForm() : this.renderViewForm()}
             </div>
         );
     }
--- Frontend/src/js/components/PersonProfile/views/PersonalInformation.react.js
+++ Frontend/src/js/components/PersonProfile/views/PersonalInformation.react.js
@@ -9,7 +9,54 @@ import PersonContacts from './PersonContacts.react.js';
 
 export default class PersonalInformation extends React.Component {
 
-    render() {
+    toggleEditMode;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            editMode: false
+        };
+
+        this.toggleEditMode = () => this.setState({
+            editMode: !this.state.editMode
+        });
+    }
+
+    renderViewForm() {
+        let inputLabelContainerStyle = {
+            textAlign: "right",
+            fontSize: "12px"
+        };
+
+        let valueLabel = {
+            textAlign: "left",
+            fontSize: "12px",
+            borderBottom: "solid 1px lightgrey"
+        };
+
+        return (
+            <div className="panel-body">
+                <form className="form-horizontal">
+                    <form className="form-horizontal">
+                        <div className="form-group">
+                            <div className="col-sm-2" style={inputLabelContainerStyle}><label>Anrede:</label></div>
+                            <div className="col-sm-9" style={valueLabel}>---</div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-2" style={inputLabelContainerStyle}><label>Vorname:</label></div>
+                            <div className="col-sm-9" style={valueLabel}>---</div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-2" style={inputLabelContainerStyle}><label>Nachname:</label></div>
+                            <div className="col-sm-9" style={valueLabel}>---</div>
+                        </div>
+                    </form>
+                </form>
+            </div>
+        );
+    }
+
+    renderEditForm() {
         let inputLabelContainerStyle = {
             textAlign: "right",
             paddingTop: "6px",
@@ -17,25 +64,37 @@ export default class PersonalInformation extends React.Component {
         };
 
         return (
+            <div className="panel-body">
+                <form className="form-horizontal">
+
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Anrede:</label></div>
+                        <div className="col-sm-9"><ComboBox width="100%" data={['Herr', 'Frau']}/></div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Vorname:</label></div>
+                        <div className="col-sm-9"><InputField width="100%"/></div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-2" style={inputLabelContainerStyle}><label>Nachname:</label></div>
+                        <div className="col-sm-9"><InputField width="100%"/></div>
+                    </div>
+                </form>
+                <PersonContacts {...this.props}/>
+            </div>
+        );
+    }
+
+    render() {
+        const switchEditModeIconClass = (this.state.editMode === true) ? "glyphicon glyphicon-eye-open" : "glyphicon glyphicon-pencil";
+
+        return (
             <div className="panel panel-success" style={{width: "1000px", margin: "0 auto"}}>
-                <div className="panel-heading"><strong>Persönliche Kontaktinformationen</strong></div>
-                <div className="panel-body">
-                    <form className="form-horizontal">
-                        <div className="form-group">
-                            <div className="col-sm-2" style={inputLabelContainerStyle}><label>Anrede:</label></div>
-                            <div className="col-sm-9"><ComboBox width="100%" data={['Herr', 'Frau']}/></div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-sm-2" style={inputLabelContainerStyle}><label>Vorname:</label></div>
-                            <div className="col-sm-9"><InputField width="100%"/></div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-sm-2" style={inputLabelContainerStyle}><label>Nachname:</label></div>
-                            <div className="col-sm-9"><InputField width="100%"/></div>
-                        </div>
-                    </form>
-                    <PersonContacts {...this.props}/>
+                <div className="panel-heading" style={{position: "relative"}}>
+                    <strong>Persönliche Kontaktinformationen</strong>
+                    <div className={switchEditModeIconClass} style={{position: "absolute", fontSize: "16px", top: "14px", right: "14px"}} onClick={this.toggleEditMode}/>
                 </div>
+                {(this.state.editMode === true) ? this.renderEditForm() : this.renderViewForm()}
             </div>
         );
     }
