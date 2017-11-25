1af58bdc8b92aa1838e89827f23a9753c378f84c
br4ska <sven.steffan@gmail.com>
2017-11-25 20:59:57 +0100

    - timeseries-chart now loads data from backend and reloads data if the date changes

--- Frontend/src/js/components/PersonProfile/views/PersonPerformance.react.js
+++ Frontend/src/js/components/PersonProfile/views/PersonPerformance.react.js
@@ -3,15 +3,35 @@
  */
 import React from 'react';
 
-import PerformanceChart from '../../../widgets/view/SimpleLineChart.react.js';
+import ComboBox from '../../../widgets/control/ComboBox.react.js';
+import DateInputField from '../../../widgets/control/DateInputField.react.js';
+import TimeseriesChart from '../../../widgets/view/SimpleLineChart.react.js';
 
 export default class PersonalInformation extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            date: "02.01.2017"
+        };
+
+        this.dateChanged = function(newDate) {
+            debugger;
+            this.setState({
+                date: newDate
+            });
+        }
     }
 
     render() {
+        const date = new Date();
+        const inputLabelContainerStyle = {
+            textAlign: "right",
+            paddingTop: "6px",
+            fontSize: "12px"
+        };
+
         return (
             <div className="panel panel-nevsuite" style={{marginTop: "15px"}}>
                 <div className="panel-heading">
@@ -19,8 +39,16 @@ export default class PersonalInformation extends React.Component {
                 </div>
                 <div className="panel-body">
                     <form className="form-horizontal">
-                        <div className="col-sm-10 col-sm-offset-1">
-                            <PerformanceChart/>
+                        <div className="form-group">
+                            <div className=" col-sm-10 col-sm-offset-1">
+                                <div className="col-sm-1" style={inputLabelContainerStyle}><label>Datum:</label></div>
+                                <div className="col-sm-5"><DateInputField value={date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear()} dateChanged={(newDate) => this.dateChanged(newDate)} /></div>
+                                <div className="col-sm-1" style={inputLabelContainerStyle}><label>Raster:</label></div>
+                                <div className="col-sm-5"><ComboBox data={["Tag", "Monat", "Jahr"]} value="Tag" /></div>
+                            </div>
+                        </div>
+                        <div className="col-sm-10 col-sm-offset-1" style={{marginTop: "15px"}}>
+                            <TimeseriesChart date={this.state.date}/>
                         </div>
                     </form>
                 </div>
--- Frontend/src/js/widgets/view/SimpleLineChart.react.js
+++ Frontend/src/js/widgets/view/SimpleLineChart.react.js
@@ -2,47 +2,114 @@
  * Created by Braska on 25.11.2017.
  */
 import React from "react";
-import Chart from "chart.js";
+import Chart from "../../../node_modules/chart.js/src/chart.js";
 
 export default class SimpleLineChart extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            xData: [],
+            actDate: this.props.date
+        };
     }
 
-    componentDidMount() {
+    readTimeseries() {
+        let xhttp = new XMLHttpRequest();
+
+        let data = '';
+        let xData = [];
+        let yData = [];
+
+        let date = (this.props.date !== "" && this.props.date !== undefined) ? this.props.date : "01.01.2017";
+        let dayTo = parseInt(date.split('.')[0]) + 1;
+        let timestampFrom = date.split('.')[2] + "-" + date.split('.')[1] + "-" + date.split('.')[0] + "T00:00:00Z";
+        let timestampTo = date.split('.')[2] + "-" + date.split('.')[1] + "-" + (dayTo < 10 ? "0" + dayTo : dayTo) + "T00:00:00Z";
+
+        let setState = (xData, yData) => {
+            debugger;
+            this.setState({
+                xData : xData,
+                yData : yData
+            })
+        };
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                data = JSON.parse(this.responseText);
+                xData.push('00:00');
+                yData.push(data.timeseries.valueMap[0].value);
+                data.timeseries.valueMap.forEach(function(tuple) {
+                    let time = tuple.timestamp.split('T')[1].split('Z')[0];
+                    xData.push(time.split(':')[0] + ':' + time.split(':')[1]);
+                    yData.push(tuple.value);
+                });
+                setState(xData, yData);
+            } else if (this.readyState == 4) {
+                data = [];
+            }
+        };
+        xhttp.open("GET", "http://localhost:8080/timeseries/DomainService/timeseries/" + timestampFrom + "/" + timestampTo, true);
+        xhttp.send();
+    }
+
+    createChart() {
         let $ctx = document.getElementById('simpleLineChart').getContext('2d');
         new Chart($ctx, {
             type: 'line',
             data: {
-                labels: ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"],
-                datasets: [
-                    {   label: "Soll",
-                        backgroundColor: 'transparent',
-                        borderColor: 'rgb(7, 65, 47)',
-                        data: [8, 8, 8, 8, 8],
-                        steppedLine: true
-                    },
-                    {
-                        label: "Ist",
-                        backgroundColor: 'rgba(134, 191, 160, 0.3)',
-                        borderColor: 'rgb(66, 147, 102)',
-                        data: [7, 9.5, 7.5, 10, 8],
-                        steppedLine: false
-                    }]
+                labels: this.state.xData,
+                datasets: [{
+                    label: "Timeseries",
+                    backgroundColor: 'rgba(134, 191, 160, 0.3)',
+                    borderColor: 'rgb(66, 147, 102)',
+                    data: this.state.yData,
+                    steppedLine: true,
+                    lineTension: 0,
+                    pointRadius: 0
+                }]
             },
             options: {
+                animation: {
+                    duration: 0,
+                },
+                legend: {
+                    display: false
+                },
                 scales: {
                     yAxes: [{
                         ticks: {
-                            max: 12,
+                            max: 100,
                             min: 0,
-                            stepSize: 1
+                            stepSize: 10
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            autoSkip: true,
+                            maxTicksLimit: 24
                         }
                     }]
                 }
             }
         });
+    }
+
+    componentDidUpdate() {
+        this.createChart();
+        if ( this.state.actDate !== this.props.date ) {
+            this.setState({
+                xData: this.state.xData,
+                actDate: this.props.date
+            });
+            this.readTimeseries();
+        }
+    }
+
+    componentDidMount() {
+        this.createChart();
+        this.readTimeseries();
     }
 
     render() {
