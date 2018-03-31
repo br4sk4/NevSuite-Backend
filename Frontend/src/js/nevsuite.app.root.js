fbc13c2935f330cd530c5f759c989d5602d0f89a
br4sk4 <sven.steffan@gmail.com>
2018-03-31 22:20:44 +0200

    Removed frontend from this repository as the frontend was moved to its own repository

--- Frontend/src/js/nevsuite.app.root.js
+++
@@ -1,8 +0,0 @@
-﻿'use strict';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import NevSuiteApp from './nevsuite.app.container.js';
-
-ReactDOM.render(<NevSuiteApp/>, document.querySelector("application"));
